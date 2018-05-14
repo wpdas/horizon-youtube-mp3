67f714ecb843b8bb87408cdd578c980cfed020f9
Wpdas <wpdas@yahoo.com.br>
2018-05-14 23:48:44 +0100

    Method getInfo(...) return new format of attribute videoThumbList. It will return an array of objects contain url, width and height.

--- examples/videoinfo.js
+++ examples/videoinfo.js
@@ -8,11 +8,12 @@ horizon.getInfo('http://youtube.com/watch?v=NEA0BLnpOtg', function(err, e){
   log.info(e);
 
   /**
-     * Will Return:
+     * It'll return:
      *
      * { isValid: true,
      *   videoName: 'OZIELZINHO - TOP GEAR 2.0',
      *   videoThumb: 'https://i.ytimg.com/vi/NEA0BLnpOtg/hqdefault.jpg?custom=true&w=320&h=180&stc=true&jpg444=true&jpgq=90&sp=68&sigh=FoGsoudXCGPU-Fb6epRh1eIzVDs',
+     *   videoThumbList: [{url, width, height}, {...}...]
      *   videoTime: '2:35',
       *  videoFile: 'https://....'}
      */
